f00234a3dc4e433b933c71c4a30515dd9cbd2570
freitassantos <rodrigo.santos203@fatec.sp.gov.br>
2020-10-29 20:30:46 -0300

    instalação do slugify e criado o metodo create category na database

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("./Category");
+const sluify = require("slugify");
+const { default: slugify } = require("slugify");
 
 router.get("/categories",(req,res) =>{
     res.send("Rota de categorias")
 })
 
 router.get("/admin/categories/new",(req,res) =>{
-    res.send("Rota para criar uma nova categoria")
+    res.render("admin/categories/new");
 })
+
+router.post("/categories/save",(req,res)=>{
+    var title = req.body.title;
+    if (title != undefined){
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then( ()=>{
+            res.redirect("/");
+        })
+    }else{  
+        res.redirect("admin/categories/new");
+    }
+});
 
 module.exports = router;
