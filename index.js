0147251925954c2da89f6110d7a14f3bf3807a9f
freitassantos <rodrigo.santos203@fatec.sp.gov.br>
2020-10-29 21:44:59 -0300

    implementado favicon

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
+app.use("/public", express.static('public'));
+
 const connection = require("./database/database");
 const Article = require("./articles/Article");
 const Category = require("./categories/Category");
 
 const categoriesController = require("./categories/categoriesController");
 const articlesController = require("./articles/articlesController")
-
-
 
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:false}));
